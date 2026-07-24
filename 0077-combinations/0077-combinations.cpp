93135ab143eda181202675bff531982f75e1a18d
Aditya Raj <araj44124@gmail.com>
2026-07-24 12:27:50 +0530

    Time: 38 ms (72.51%), Space: 62.5 MB (86.12%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    void f(int st, vector<int> &comb, vector<vector<int>> &res, int n, int k){
-        if(comb.size() == k) {
-            res.push_back(comb);
+    void f(int st, vector<int>& comb, vector<vector<int>>& ans, int n, int k) {
+        if (comb.size() == k) {
+            ans.push_back(comb);
             return;
         }
 
-        for(int i=st; i<=n; i++){
+        for (int i = st; i <= n; i++) {
             comb.push_back(i);
-            f(i+1, comb,res, n, k);
+            f(i + 1, comb, ans, n, k);
             comb.pop_back();
         }
     }
-    
+
     vector<vector<int>> combine(int n, int k) {
 
-        vector<vector<int>> res;
+        vector<vector<int>> ans;
         vector<int> comb;
 
-        f(1, comb, res, n, k);
-        return res;
+        f(1, comb, ans, n, k);
+        return ans;
     }
 };
