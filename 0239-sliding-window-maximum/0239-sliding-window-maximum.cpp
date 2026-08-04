e27f5c831c5b3849abbe96faa8808d7421923a52
Aditya Raj <araj44124@gmail.com>
2026-08-04 16:51:53 +0530

    Time: 25 ms (47.87%), Space: 139.3 MB (46.87%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,36 +1,34 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        int n = nums.size();
+        int n=nums.size();
+        deque<int> dq;
         vector<int> ans;
-        multiset<int> ms;
 
-        // Insert first window
+        // 1st window
         for (int i = 0; i < k; i++) {
-            ms.insert(nums[i]);
+            while (dq.size() > 0 && nums[dq.back()] <= nums[i]) {
+                dq.pop_back();
+            }
+            dq.push_back(i);
         }
 
-        // Maximum of first window
-        ans.push_back(*prev(ms.end()));
+        for(int i=k; i<n; i++){
+            ans.push_back(nums[dq.front()]);
 
-        int left = 0;
-        int right = k;
+            //remove ele which are not part of curr win
+            while(dq.size()>0 && dq.front()<=i-k){
+                dq.pop_front();
+            }
 
-        while (right < n) {
+            //remove smaller val
+            while (dq.size() > 0 && nums[dq.back()] <= nums[i]) {
+                dq.pop_back();
+            }
 
-            // Remove outgoing element
-            ms.erase(ms.find(nums[left]));
-
-            // Insert incoming element
-            ms.insert(nums[right]);
-
-            // Maximum of current window
-            ans.push_back(*prev(ms.end()));
-
-            left++;
-            right++;
+            dq.push_back(i);
         }
-
+        ans.push_back(nums[dq.front()]);
         return ans;
     }
 };
