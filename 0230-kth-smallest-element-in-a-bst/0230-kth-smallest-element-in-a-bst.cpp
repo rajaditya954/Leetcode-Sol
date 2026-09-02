095863d0bab7dcca9faced0132e5affa2e2933f7
Aditya Raj <araj44124@gmail.com>
2026-09-02 17:40:58 +0530

    Time: 0 ms (100%), Space: 24.4 MB (41.36%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -6,32 +6,34 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    int prevOrder=0;
+    int prevOrder = 0;
     int kthSmallest(TreeNode* root, int k) {
-        if(root==NULL) return -1;
+        if (root == NULL)
+            return -1;
 
-        if(root->left!=NULL){
-            int leftAns=kthSmallest(root->left, k);
-            if(leftAns!=-1) return leftAns;
+        if (root->left != NULL) {
+            int leftAns = kthSmallest(root->left, k);
+            if (leftAns != -1)
+                return leftAns;
         }
 
-        if(prevOrder+1==k){
+        if (prevOrder + 1 == k) {
             return root->val;
         }
-        prevOrder=prevOrder+1;
+        prevOrder = prevOrder + 1;
 
-        if(root->right!=NULL){
-            int rightAns=kthSmallest(root->right, k);
-            if(rightAns!=-1) return rightAns;
+        if (root->right != NULL) {
+            int rightAns = kthSmallest(root->right, k);
+            if (rightAns != -1)
+                return rightAns;
         }
 
         return -1;
-
-
     }
 };
